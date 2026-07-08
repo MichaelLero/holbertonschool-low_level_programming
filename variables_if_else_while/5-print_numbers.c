e3ab997dc4dad3d97104d734c6f8eb7a38602c97
MichaelLero <14804@holbertonstudents.com>
2026-07-08 06:41:27 +0000

    prints all single digit numbers followed by a new line

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; c <= '9'; n++)
+	{
+		printf("%d", n);
+	}
+
+	printf('\n');
+
+	return (0);
+}
