f38453bf048312c17613d3943441aec6b92c2c5e
MichaelLero <14804@holbertonstudents.com>
2026-07-08 07:08:37 +0000

    prints all single digit numbers using putchar, following a new line

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers using putchar
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
