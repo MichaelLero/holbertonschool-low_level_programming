491e472bc9e4b68de5bb34d2275c48eab8ada19d
MichaelLero <14804@holbertonstudents.com>
2026-07-09 11:36:39 +0000

    prints numbers from n to 98

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_to_98 - prints numbers from n to 98
+ * @n: starting number
+ *
+ * Return: Nothing
+ */
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+		printf("%d, ", n);
+
+		if (n < 98)
+		{
+			n++;
+		}
+		else
+		{
+			n--;
+		}
+	}
+
+	prirntf("%d\n", n);
+}
