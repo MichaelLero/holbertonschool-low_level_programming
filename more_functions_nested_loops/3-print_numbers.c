15cddbbad9d58a56cc2d7f256e93ae0c72cfa944
MichaelLero <14804@holbertonstudents.com>
2026-07-10 10:55:16 +0000

    functions prints numbers 0 to 9

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main"
+/*
+ * print_numbers - brings numbers 0-9
+ *
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while(i <= 9)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
+}
