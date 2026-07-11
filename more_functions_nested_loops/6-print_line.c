dbac52fcb8507ad3930b9aaa365f3a49f0241afc
MichaelLero <14804@holbertonstudents.com>
2026-07-11 06:17:18 +0000

    draws a straight line in the terminal

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -4,12 +4,12 @@
  * @n: amount of times we want _ printed
  *
  * Return: Nothing
- */
+ */i
 void print_line(int n)
 {
 	while (n > 0)
 	{
-		_puchar('_');
+		_putchar('_');
 		n--;
 	}
 	_putchar('\n');
