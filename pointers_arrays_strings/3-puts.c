1d49d40ee00413e0e45954ce921af6833c696829
MichaelLero <14804@holbertonstudents.com>
2026-07-14 15:25:12 +1000

    prints a string followed by a new line

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- *_puts -
- *@str:
+ *_puts - prints a string, followed by a new line
+ *@str: string to print
  *
  * Return: Nothing
  */
 void _puts(char *str)
-{	
+{
 	int counter;
 
 	counter = 0;
