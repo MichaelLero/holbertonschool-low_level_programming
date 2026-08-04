40d2603b4e7c126f0eec9e0757861dda7d93f1c1
MichaelLero <14804@holbertonstudents.com>
2026-08-04 18:01:31 +1000

    Prints numbers, followed by a new line

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,87 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * print_number - prints an integer
+ * @number: integer to print
+ *
+ * Return: nothing
+ */
+void print_number(int number)
+{
+	int digits[10];
+	int i;
+
+	i = 0;
+
+	if (number == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
+	while (number > 0)
+	{
+		digits[i] = number % 10;
+		number = number / 10;
+		i++;
+	}
+
+	while (i > 0)
+	{
+		i--;
+		_putchar(digits[i] + '0');
+	}
+
+}
+
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: character separating between numbers
+ * @n: number of integers passed in the function
+ *
+ * Return: nothing
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int counter;
+	int numbers;
+	va_list args;
+	int i;
+
+	counter = 0;
+	numbers = 0;
+
+	if (n == 0)
+	{
+		return;
+	}
+
+	va_start(args, n);
+
+	while (counter < n)
+	{
+		if (counter > 0)
+		{
+			i = 0;
+
+			while (separator[i] != '\0')
+			{
+				_putchar(separator[i]);
+				i++;
+			}
+		}
+
+		numbers = va_arg(args, int);
+
+		print_number(number);
+
+		counter++;
+	}
+
+	va_end(args);
+
+	_putchar('\n');
+
+}
