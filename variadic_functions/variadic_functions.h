3f052c14f0322e42d24f01380dcfec85526d34dc
MichaelLero <14804@holbertonstudents.com>
2026-08-05 17:01:55 +1000

    Functions

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -8,5 +8,7 @@ int _putchar(char c);
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_number(int number);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
