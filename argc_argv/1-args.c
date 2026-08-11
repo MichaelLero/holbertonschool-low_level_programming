8a2b1b9001fbd40cb5c6b8ba4432a41ec515785b
MichaelLero <14804@holbertonstudents.com>
2026-08-11 16:36:52 +1000

    Prints the number of arguements passed by it, followed by a new line

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguements passed, followed by a new line
+ * @argc: counts the number of arguements
+ * @argv: unused
+ *
+ * Return: Success (0)
+ *
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+
+	return (0);
+}
