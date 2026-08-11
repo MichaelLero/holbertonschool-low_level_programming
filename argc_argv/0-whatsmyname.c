2b04430eefd4a209c6a4036827f47b130f9b1ec9
MichaelLero <14804@holbertonstudents.com>
2026-08-11 16:14:22 +1000

    Prints program name followed by a new line

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints a programs name, followed by a new line
+ * @argc: unused
+ * @argv: give knowledge on what arguements are present
+ *
+ * Return: Success (0) 
+ */
+int main(int argc, char *argv[])
+{
+	void(argc);
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
