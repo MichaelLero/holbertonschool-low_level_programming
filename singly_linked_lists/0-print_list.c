b933a5ec8b2e35e43fd1aaa09d30a47d1d2e2df2
MichaelLero <14804@holbertonstudents.com>
2026-08-17 21:34:22 +1000

    prints all the elements of a list_t list

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include "lists.h"
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the list_t list
@@ -9,21 +9,21 @@
 size_t print_list(const list_t *h)
 {
 	const list_t *current;
-	int counter;
+	size_t counter;
 
 	current = h;
 	counter = 0;
 
-	while (h != NULL)
+	while (current != NULL)
 	{
 		if (current->str == NULL)
 		{
-			print("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		}
 
 		else
 		{
-			printf("[%u} %s\n", current->len, current->str);
+			printf("[%u] %s\n", current->len, current->str);
 		}
 
 		counter++;
