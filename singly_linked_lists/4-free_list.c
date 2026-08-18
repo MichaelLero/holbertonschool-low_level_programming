d1149c2b79cff54d32f920934ba2b3eab865e399
MichaelLero <14804@holbertonstudents.com>
2026-08-18 17:20:00 +1000

    frees a list_t list

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the head of the list_t list
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp;
+
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
+
