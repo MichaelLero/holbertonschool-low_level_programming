d22f9b4d39799e3eab421281c19b01d57351c99d
MichaelLero <14804@holbertonstudents.com>
2026-08-21 23:27:40 +1000

    returns the factorial of a given number

---
+++ recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the number
+ *
+ * Return: the factorial of n, or -1 if n is less than 0
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+
+	return (n * factorial(n - 1));
+}
